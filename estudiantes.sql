5058d0c8c3ff10500ed984f616a8782ccb279f8a
Segundo <segundogaldames@gmail.com>
2021-09-25 21:22:05 -0300

    Consultas realizadas el 25 de septiembre

--- estudiantes.sql
+++ estudiantes.sql
@@ -1,5 +1,9 @@
-INSERT INTO estudiantes(rut, nombre, fecha_nac, email, direccion, comuna_id) VALUES('20963876-4','Francisco Perez','1998-12-15','fperez@gmail.com', 'Las Animas 4',9);
-INSERT INTO estudiantes(rut, nombre, fecha_nac, email, direccion, comuna_id) VALUES('16987230-K','Eunice Jara','1988-07-10','e.jara@hotmail.com', 'Sargento Aldea 415',11);
-INSERT INTO estudiantes(rut, nombre, fecha_nac, email, direccion, comuna_id) VALUES('19800741-3','Juan Morales','1990-03-22','jmoraless@gmail.com', 'Chacabuco 903',7);
+INSERT INTO estudiantes(rut, nombre, fecha_nac, email, direccion, comuna_id) VALUES('21800741-4','Manuel Soto','2000-04-22','msoto@gmail.com', 'Las Ardenas 1291',4);
+INSERT INTO estudiantes(rut, nombre, fecha_nac, email, direccion, comuna_id) VALUES('20800741-2','Javiera Rosales','1999-10-23','j.rosales@hotmail.com', 'Carrera 18',2);
+INSERT INTO estudiantes(rut, nombre, fecha_nac, email, direccion, comuna_id) VALUES('19800001-6','Luis Caceres','1990-03-06','lcaceres@yahoo.es', 'Agustinas 1306',1);
+INSERT INTO estudiantes(rut, nombre, fecha_nac, email, direccion, comuna_id) VALUES('20800741-K','Maria Torres','2001-11-22','mariatorres@gmail.com', 'Huerfanos 9999',1);
+INSERT INTO estudiantes(rut, nombre, fecha_nac, email, direccion, comuna_id) VALUES('22805741-8','Samuel Tapia','2000-09-01','stapia@gmail.com', 'Manuel Rodriguez 325',23);
+INSERT INTO estudiantes(rut, nombre, fecha_nac, email, direccion, comuna_id) VALUES('18800001-3','Oscar Maturana','1998-04-15','o.mat@hotmail.com', 'Ohiggins 1593',3);
+
 
 
---
+++ matriculas.sql
@@ -0,0 +1,8 @@
+INSERT INTO matriculas (fecha_matricula, valor, regimen, carrera_id, estudiante_id, nivel_id) VALUES(now(), 55000, 3, 3, 3, 3);
+INSERT INTO matriculas (fecha_matricula, valor, regimen, carrera_id, estudiante_id, nivel_id) VALUES(now(), 60000, 2, 1, 6, 4);
+INSERT INTO matriculas (fecha_matricula, valor, regimen, carrera_id, estudiante_id, nivel_id) VALUES(now(), 85000, 2, 4, 7, 2);
+INSERT INTO matriculas (fecha_matricula, valor, regimen, carrera_id, estudiante_id, nivel_id) VALUES(now(), 65000, 2, 3, 8, 5);
+INSERT INTO matriculas (fecha_matricula, valor, regimen, carrera_id, estudiante_id, nivel_id) VALUES(now(), 0, 2, 2, 4, 6);
+INSERT INTO matriculas (fecha_matricula, valor, regimen, carrera_id, estudiante_id, nivel_id) VALUES(now(), 120000, 2, 4, 9, 7);
+INSERT INTO matriculas (fecha_matricula, valor, regimen, carrera_id, estudiante_id, nivel_id) VALUES(now(), 45000, 2, 2, 5, 2);
+
